7cb62b8e301505b3d21dda8102a96937cf8a1db9
Firman Hadi P <painneon@gmail.comxy>
2019-10-17 18:17:16 +0700

    WIP: Created evaluate function for the expressions DS

--- aritmatika_sederhana/expressions.hs
+++ aritmatika_sederhana/expressions.hs
@@ -1,3 +1,8 @@
-data Expr = Const Float | Expr :+ Expr | Expr :- Expr
+data Expr = C Float | Expr :+ Expr | Expr :- Expr
             | Expr :* Expr | Expr :/ Expr
 
+evaluate (C const) = const
+evaluate (expr1 :+ expr2) = (evaluate expr1) + (evaluate expr2)
+evaluate (expr1 :- expr2) = (evaluate expr1) - (evaluate expr2)
+evaluate (expr1 :* expr2) = (evaluate expr1) * (evaluate expr2)
+evaluate (expr1 :/ expr2) = (evaluate expr1) / (evaluate expr2)
