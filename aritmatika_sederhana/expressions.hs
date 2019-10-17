6135fd2f97cbd0f7f9a651c8c88a3d6f39aa544c
Firman Hadi P <painneon@gmail.comxy>
2019-10-17 22:19:11 +0700

    WIP: Created variable symbol and symbolic substitution for expression data structure

--- aritmatika_sederhana/expressions.hs
+++ aritmatika_sederhana/expressions.hs
@@ -1,8 +1,22 @@
 data Expr = C Float | Expr :+ Expr | Expr :- Expr
             | Expr :* Expr | Expr :/ Expr
+            | V String | Let String Expr Expr
+
+-- Substitute
+substitute var expr (V var2)
+    | var == var2   = expr
+    | otherwise     = V var2
+substitute var expr (C const) = C const
+substitute var expr (expr1 :+ expr2) = (substitute var expr expr1) :+ (substitute var expr expr2)
+substitute var expr (expr1 :- expr2) = (substitute var expr expr1) :- (substitute var expr expr2)
+substitute var expr (expr1 :* expr2) = (substitute var expr expr1) :* (substitute var expr expr2)
+substitute var expr (expr1 :/ expr2) = (substitute var expr expr1) :/ (substitute var expr expr2)
+substitute var expr (Let var2 expr2 expr3) = Let var2 expr2 (substitute var expr expr3)
+
 
 evaluate (C const) = const
 evaluate (expr1 :+ expr2) = (evaluate expr1) + (evaluate expr2)
 evaluate (expr1 :- expr2) = (evaluate expr1) - (evaluate expr2)
 evaluate (expr1 :* expr2) = (evaluate expr1) * (evaluate expr2)
 evaluate (expr1 :/ expr2) = (evaluate expr1) / (evaluate expr2)
+evaluate (Let v v_expr expr) = evaluate (substitute v v_expr expr)
