e9eea8fa0c552f9b3fd8fac885773f1fdbd4780e
Firman Hadi P <painneon@gmail.comxy>
2019-10-18 19:47:25 +0700

    WIP: Added fold for the expression tree, also added evaluate function through fold

--- aritmatika_sederhana/expressions.hs
+++ aritmatika_sederhana/expressions.hs
@@ -20,3 +20,21 @@ evaluate (expr1 :- expr2) = (evaluate expr1) - (evaluate expr2)
 evaluate (expr1 :* expr2) = (evaluate expr1) * (evaluate expr2)
 evaluate (expr1 :/ expr2) = (evaluate expr1) / (evaluate expr2)
 evaluate (Let v v_expr expr) = evaluate (substitute v v_expr expr)
+
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (C const) = fconst const
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :+ expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fadd` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :- expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fsub` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :* expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fmul` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :/ expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fdiv` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (V v) = fvar v
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (Let var expr1 expr2) = flet var expr1 expr2
+
+evaluateFold = fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet)
+                where
+                    fconst = id
+                    fadd = (+)
+                    fsub = (-)
+                    fmul = (*)
+                    fdiv = (/)
+                    fvar v = 0
+                    flet var expr1 expr2 = evaluateFold (substitute var expr1 expr2)
