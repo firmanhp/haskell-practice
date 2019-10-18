5a193aae323fd7c34e9a0454c6a05de6a8377721
Firman Hadi P <painneon@gmail.comxy>
2019-10-18 20:10:48 +0700

    Refactored the fold function definition

--- aritmatika_sederhana/expressions.hs
+++ aritmatika_sederhana/expressions.hs
@@ -22,10 +22,14 @@ evaluate (expr1 :/ expr2) = (evaluate expr1) / (evaluate expr2)
 evaluate (Let v v_expr expr) = evaluate (substitute v v_expr expr)
 
 fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (C const) = fconst const
-fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :+ expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fadd` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
-fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :- expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fsub` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
-fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :* expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fmul` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
-fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :/ expr2) = (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr1) `fdiv` (fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :+ expr2) =  let fs = (fconst,fadd,fsub,fmul,fdiv,fvar,flet) in
+                                                                fadd (fold fs expr1) (fold fs expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :- expr2) =  let fs = (fconst,fadd,fsub,fmul,fdiv,fvar,flet) in
+                                                                fsub (fold fs expr1) (fold fs expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :* expr2) =  let fs = (fconst,fadd,fsub,fmul,fdiv,fvar,flet) in
+                                                                fmul (fold fs expr1) (fold fs expr2)
+fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (expr1 :/ expr2) =  let fs = (fconst,fadd,fsub,fmul,fdiv,fvar,flet) in
+                                                                fdiv (fold fs expr1) (fold fs expr2)
 fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (V v) = fvar v
 fold (fconst,fadd,fsub,fmul,fdiv,fvar,flet) (Let var expr1 expr2) = flet var expr1 expr2
 
